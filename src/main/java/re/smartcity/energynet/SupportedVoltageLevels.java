a1e0fa86aaead506bdcc26c443fbec3d2fd11158
szukovvl <vl123@tpu.ru>
2022-08-14 18:37:11 +0700

    Влад (main)

--- src/main/java/re/smartcity/energynet/SupportedVoltageLevels.java
+++ src/main/java/re/smartcity/energynet/SupportedVoltageLevels.java
@@ -2,5 +2,5 @@ package re.smartcity.energynet;
 
 public enum SupportedVoltageLevels {
     AVG_VOLTAGE_1, // СН1 - среднее напряжение, 3-35 кВ (35)
-    LOW_VOLTAGE // НН - низкое напряжение, 0,4 кВ
+    LOW_VOLTAGE1 // НН - низкое напряжение, 0,4 кВ
 }
